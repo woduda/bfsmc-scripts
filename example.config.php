cad12892b0e122e2541c6661c543349c916e8be6
Wojciech Duda <wo.duda@gmail.com>
2019-06-20 23:19:28 +0200

    updateTitles method.

--- example.config.php
+++ example.config.php
@@ -4,4 +4,6 @@ return [
     'host' => 'localhost',
     'port' => '9876',
     'password' => '****',
+    'db_path' => '/full/path/to/db.json',
+    'bfs_path' => '/full/path/to/bfs.json',
 ];
--- run.php
+++ run.php
@@ -26,13 +26,69 @@ class Run
 
     public function main()
     {
+
         if ($this->ws->connect()) {
             $this->send("say test");
         } else {
             echo "Connection error! Check ip, pass and port.\n";
             return;
         }
+
+        $this->updateTitles();
+
         $this->ws->disconnect();
+    }
+
+
+    protected function updateTitles()
+    {
+        $db_content = @file_get_contents($this->config['db_path']);
+        $bfs_content = @file_get_contents($this->config['bfs_path']);
+
+        $awards = json_decode($db_content, true)['awards'];
+
+        if ($bfs_content) {
+            $bfs = json_decode($bfs_content, true);
+        } else {
+            $bfs = [];
+        }
+        $isset = [];
+        foreach ($awards as $id => $award) {
+            $uuid = $award['best']['uuid'] ?? false;
+            $_uuid = $bfs['titles'][$id] ?? false;
+
+            if ($uuid === $_uuid) {
+                $isset[] = $uuid;
+                continue;
+            }
+
+            if ($_uuid !== false) {
+                $this->setTitle($_uuid, ''); // clear old title
+                unset($bfs['titles'][$id]);
+            }
+            if (! in_array($uuid, $isset)) {
+                $k = array_search($uuid, $bfs['titles'] ?? []);
+                if ($k !== $id) {
+                    $this->setTitle($uuid, $award['title']);
+                    $isset[] = $uuid;
+
+                    unset($bfs['titles'][$k]);
+                    $bfs['titles'][$id] = $uuid;
+                }
+            }
+        }
+
+        @file_put_contents($this->config['bfs_path'], json_encode($bfs));
+
+        return $this;
+    }
+
+    public function setTitle($uuid, $title = null)
+    {
+        $suffix = (empty($title) ? '""' : ('" &2'.$title.'"'));
+        $cmd = "pex user $uuid suffix $suffix";
+        $this->send($cmd);
+        echo "$cmd\n";
     }
 
     public function send($cmd)
