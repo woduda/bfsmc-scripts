40d03c9887d5397ca8246b600c25adf345de9d1d
Wojciech Duda <wo.duda@gmail.com>
2019-06-20 19:19:45 +0200

    say test

--- run.php
+++ run.php
@@ -1,3 +1,44 @@
 <?php
 
-echo "Start!";
+require_once("WebsenderAPI.php");
+
+class Run
+{
+    private $config;
+
+    private $ws;
+
+    public function __construct()
+    {
+        $this->config = $this->config();
+
+        $this->ws = new WebsenderAPI(
+            $this->config['host'],
+            $this->config['password'],
+            $this->config['port']
+        );
+    }
+
+    protected function config()
+    {
+        return include "./config.php";
+    }
+
+    public function main()
+    {
+        if ($this->ws->connect()) {
+            $this->send("say test");
+        } else {
+            echo "Connection error! Check ip, pass and port.\n";
+            return;
+        }
+        $this->ws->disconnect();
+    }
+
+    public function send($cmd)
+    {
+        $this->ws->sendCommand($cmd);
+    }
+}
+
+(new Run())->main();
